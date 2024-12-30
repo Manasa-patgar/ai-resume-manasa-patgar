2cf02e006bc1be0d0cc5003719765d4880c9bc89
Manasa-patgar <manasapatgar95@gmail.com>
2024-12-29 22:01:39 -0500

    Deploy build folder to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'manasa_patgar_portfolio',
+  base: '/manasa_patgar_portfolio/',
   plugins: [react()],
 })
